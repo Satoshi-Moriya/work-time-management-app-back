21fd89f82822706b9384fcf7dfc6200037abccb5
Satoshi-Moriya <moriyasatoshi1994@gmail.com>
2023-06-25 16:01:09 +0900

    最初の環境構築完了

---
+++ src/main/kotlin/com/example/worktimemanagement/WorkTimeManagementApplication.kt
@@ -0,0 +1,11 @@
+package com.example.worktimemanagement
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkTimeManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkTimeManagementApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/worktimemanagement/WorkTimeManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.worktimemanagement
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkTimeManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
